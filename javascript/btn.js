1b4dc9c0cf57bb447198193cdb7acb21b02306bc
AnuRobo <37725645+AnuRobo@users.noreply.github.com>
2021-10-22 01:09:34 +0530

    Update btn.js

--- javascript/btn.js
+++ javascript/btn.js
@@ -13,30 +13,69 @@ const menu4 = document.querySelector("#menu4");
 const menu5 = document.querySelector("#menu5");
 const menu6 = document.querySelector("#menu6");
 
+
+btn1.addEventListener("click",function(){
+    menu1.style.display = "flex";
+    menu2.style.display = "flex";
+    menu3.style.display = "flex";
+    menu4.style.display = "flex";
+    menu5.style.display = "flex";
+    menu6.style.display = "flex";
+});
 btn2.addEventListener("click",function(){
+    menu1.style.display = "flex";
     menu2.style.display = "none";
+    menu3.style.display = "flex";
+    menu4.style.display = "flex";
     menu5.style.display = "none";
+    menu6.style.display = "flex";
+    // menu2.style.display = "none";
+    // menu5.style.display = "none";
 });
 btn3.addEventListener("click",function(){
     menu1.style.display = "none";
     menu2.style.display = "none";
     menu3.style.display = "none";
+    menu4.style.display = "flex";
+    menu5.style.display = "flex";
     menu6.style.display = "none";
+    // menu1.style.display = "none";
+    // menu2.style.display = "none";
+    // menu3.style.display = "none";
+    // menu6.style.display = "none";
 });
 btn4.addEventListener("click",function(){
     menu1.style.display = "none";
+    menu2.style.display = "flex";
     menu3.style.display = "none";
+    menu4.style.display = "flex";
     menu5.style.display = "none";
     menu6.style.display = "none";
+    // menu1.style.display = "none";
+    // menu3.style.display = "none";
+    // menu5.style.display = "none";
+    // menu6.style.display = "none";
 });
 btn5.addEventListener("click",function(){
+    menu1.style.display = "flex";
     menu2.style.display = "none";
     menu3.style.display = "none";
     menu4.style.display = "none";
+    menu5.style.display = "flex";
     menu6.style.display = "none";
+    // menu2.style.display = "none";
+    // menu3.style.display = "none";
+    // menu4.style.display = "none";
+    // menu6.style.display = "none";
 });
 btn6.addEventListener("click",function(){
+    menu1.style.display = "flex";
+    menu2.style.display = "flex";
     menu3.style.display = "none";
+    menu4.style.display = "flex";
     menu5.style.display = "none";
     menu6.style.display = "none";
+    // menu3.style.display = "none";
+    // menu5.style.display = "none";
+    // menu6.style.display = "none";
 });
